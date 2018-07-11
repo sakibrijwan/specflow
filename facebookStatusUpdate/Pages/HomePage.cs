26aee63258c33407caba60507c8c28a52cfc45d4
Sakib <sakib23@gmail.com>
2018-07-11 14:44:23 +0600

    added stories

---
+++ facebookStatusUpdate/Pages/HomePage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using SeleniumExtras.PageObjects;
+
+namespace facebookStatusUpdate.Pages
+{
+    class HomePage
+    {
+
+        private IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.LinkText, Using = "Page")]
+        [CacheLookup]
+        public IWebElement Stories{ get; set; }
+
+        public void ClickStories()
+        {
+            Stories.Click();
+        }
+    }
+}
